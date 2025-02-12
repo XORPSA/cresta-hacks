d7443cda0c47e4baa81636c42b09032f3fdb42e1
XORPSA <christopher.jiang@cresta.ai>
2025-02-12 10:31:06 -0500

    set up auth for cresta

--- backend/settings.py
+++ backend/settings.py
@@ -1,8 +1,18 @@
 import os
+import json
 from pydantic import BaseModel, Field
 
 class OpenAICredentials(BaseModel):
-    apiKey: str = Field(default_factor=lambda: os.getenv("OPENAI_API_KEY"))
+    apiKey: str = Field(default_factory=lambda: os.getenv("OPENAI_API_KEY"))
+
+class AuthConfig(BaseModel):
+    jwt_secret: str = Field(default_factory=lambda: os.getenv("JWT_SECRET_API"))
+    config_service_addr: str = Field(default_factory=lambda: os.getenv("CONFIG_SERVICE_ADDR"))
+    config_use_secure_channel: bool = Field(
+        default_factory=lambda: os.getenv("CONFIG_USE_SECURE_CHANNEL", "false").lower() == "true"
+    )
 
 class Settings(BaseModel):
     openai_credentials: OpenAICredentials = Field(default_factory=OpenAICredentials)
+    auth_config: AuthConfig = Field(default_factory=AuthConfig)
+
